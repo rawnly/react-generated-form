db0b3e7dc681bcd5952e8e740bc23e55da627529
Rawnly <Rawnly@users.noreply.github.com>
2021-03-16 10:57:45 +0000

    deploy: 220e20ffcdb386c511c4d59f5c34375c1e712a2e

---
+++ _next/static/gyCjo7U2m3_TSt4ex3uiR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-71bb4c4d40c72dbaf156.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b41df3f17bf097679917.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
